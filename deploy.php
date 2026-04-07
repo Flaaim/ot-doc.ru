1e7a6605b855002f086ffb30afbb8a719d74a09f
Flaaim <flaaim@list.ru>
2026-04-07 16:05:40 +1000

    Fix deployer sharing config/common

--- deploy.php
+++ deploy.php
@@ -3,19 +3,33 @@
 namespace Deployer;
 
 require 'recipe/common.php';
+set('application', 'ot-doc');
+set('git_ssh_command', 'ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa');
 
 // Config
 // Укажите URL вашего репозитория (например: git@github.com:username/repo.git)
-set('repository', 'YOUR_GIT_REPOSITORY_URL');
+set('repository', 'https://github.com/Flaaim/ot-doc.ru');
+set('php_version', '8.2');
+set('bin/php', '/opt/php/8.2/bin/php');
+set('writable_mode', 'chmod');
+
+
+    host('ot-doc')
+        ->set('hostname', '31.31.198.114')
+        ->set('port', 22)
+        ->set('remote_user', 'u1656040')
+        ->set('deploy_path', '~/www/ot-doc.ru')
+        ->set('branch', 'master');
+
 
 // Общие файлы, которые не должны перезаписываться при деплое (например, конфигурация БД)
-set('shared_files', ['.env']);
+set('shared_files', ['config/common/.env']);
 
 // Общие директории
 set('shared_dirs', [
     'public/upload',
     'logs',
-    'cache',
+    'cache'
 ]);
 
 // Директории, доступные для записи
@@ -25,29 +39,29 @@ set('writable_dirs', [
     'cache',
 ]);
 
-// Если у вас shared хостинг не поддерживает sudo/chown, 
-// устанавливаем режим writable_mode в chmod или отключаем
-set('writable_mode', 'chmod'); 
+    set('bin/composer', '{{bin/php}} {{deploy_path}}/composer.phar');
+    set('composer_options', '--no-dev --optimize-autoloader --no-progress --no-interaction --no-scripts');
 
-// Host config
-host('vash_domen_ili_ip')
-    ->set('remote_user', 'vash_ssh_логин')
-    ->set('port', 22) // Укажите порт, если отличается
-    ->set('deploy_path', '~/deploy_folder') // Путь на сервере, куда будет деплоиться проект
-    ->set('identity_file', '~/.ssh/id_rsa'); // Опционально, путь до SSH ключа, если используете
-
-// Tasks
-task('deploy:vendors', function () {
-    cd('{{release_path}}');
-    run('composer install --no-dev --optimize-autoloader --no-interaction --quiet');
+// Скачиваем composer.phar, если его нет
+task('check:composer', function () {
+    if (!test('[ -f {{deploy_path}}/composer.phar ]')) {
+        run('cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}');
+    }
 });
 
-// Задача для создания симлинка public диры к web-root shared хостинга (например: public_html или www)
+before('deploy:vendors', 'check:composer');
+
+task('deploy:vendors', function () {
+    cd('{{release_path}}');
+    run('{{bin/composer}} install {{composer_options}}');
+});
+
+// Задача для создания симлинка public (корневой директории сайта)
 task('deploy:symlink_public', function () {
-    // ВАЖНО: Раскомментируйте и измените пути под ваш хостинг
-    // Это свяжет папку public релиза с публичной папкой сервера
-    // $publicHtml = '~/public_html';
-    // run("ln -sfn {{deploy_path}}/current/public $publicHtml");
+    // Поскольку у вас путь до публичной директории /www/ot-doc.ru/public,
+    // а деплоер кладет актуальный релиз в /www/ot-doc.ru/current/public,
+    // нам нужно удалить родную папку public (или симлинк) и создать новый симлинк:
+    run('cd {{deploy_path}} && rm -rf public && ln -sfn current/public public');
 });
 
 // Основной процесс деплоя
@@ -58,7 +72,7 @@ task('deploy', [
 ]);
 
 after('deploy:success', 'deploy:unlock');
-// after('deploy:success', 'deploy:symlink_public'); // Вызываем задачу симлинка после успеха
+after('deploy:success', 'deploy:symlink_public'); // Вызываем задачу симлинка после успеха
 
 // Разблокировка деплоя при ошибке
 fail('deploy', 'deploy:unlock');
